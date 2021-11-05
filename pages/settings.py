0e14902162bb8a2f31fee023747723b699cd91cb
Snipta Mallick <mallicksnip@gmail.com>
2021-11-05 16:05:40 -0500

    update settings

--- pages/settings.py
+++ pages/settings.py
@@ -2,4 +2,19 @@ import streamlit as st
 
 
 def app():
-    st.write("Settings")
+    st.write("Profile Settings")
+
+    photo = st.file_uploader("Upload a Photo")
+
+    firstname = st.text_input("First Name")
+
+    lastname = st.text_input("Last Name")
+
+    height =  st.text_input("Height")
+
+    weight =  st.number_input("Weight")
+
+
+    st.write("Daily Goals")
+
+    distance =  st.number_input("Distance")
